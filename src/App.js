491bdac238743504ca55d9a125fb21e526c6ef7d
Wilder <friendwilder@yahoo.com.mx>
2021-01-11 23:23:53 +0000

    Original version

--- src/App.js
+++ src/App.js
@@ -6,57 +6,10 @@ const value1 = Math.floor(Math.random() * 100);
 const value2 = Math.floor(Math.random() * 100);
 const value3 = Math.floor(Math.random() * 100);
 const proposedAnswer = Math.floor(Math.random() * 3) + value1 + value2 + value3;
-let numQuestions = 0;
-let numCorrect = 0;
+const numQuestions = 0;
+const numCorrect = 0;
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: 'Initial State',
-      numQuestions: numQuestions,
-      numCorrect: numCorrect
-    };
-    this.handleTrueClick = this.handleTrueClick.bind(this);
-    this.handleFalseClick = this.handleFalseClick.bind(this);
-
-  }
-  handleTrueClick() {
-    if (value1 + value2 + value3 === proposedAnswer) {
-    	numCorrect += 1;
-		numQuestions += 1;
-    	this.setState({
-      		name: "React Rocks!",
-      		numQuestions: numQuestions,
-      		numCorrect: numCorrect
-    	})      
-    } else {
-		numQuestions += 1;
-    	this.setState({
-      		name: "React Rocks!",
-      		numQuestions: numQuestions
-    	})
-  	}  
-  }
-
-  handleFalseClick() {
-    if (value1 + value2 + value3 !== proposedAnswer) {
-    	numCorrect += 1;
-		numQuestions += 1;
-    	this.setState({
-      		name: "React Rocks!",
-      		numQuestions: numQuestions,
-      		numCorrect: numCorrect
-    	})      
-    } else {
-		numQuestions += 1;
-    	this.setState({
-      		name: "React Rocks!",
-      		numQuestions: numQuestions
-    	})
-  	}  
-  }
-  
   render() {
     return (
       <div className="App">
@@ -66,15 +19,13 @@ class App extends Component {
         </header>
         <div className="game">
           <h2>Mental Math</h2>
-		  <p>{this.state.name}</p>	
           <div className="equation">
             <p className="text">{`${value1} + ${value2} + ${value3} = ${proposedAnswer}`}</p>
           </div>
-          <button onClick={this.handleTrueClick}>True</button>
-          <button onClick={this.handleFalseClick}>False</button>
-		  
+          <button>True</button>
+          <button>False</button>
           <p className="text">
-            Your Score: {this.state.numCorrect}/{this.state.numQuestions}
+            Your Score: {numCorrect}/{numQuestions}
           </p>
         </div>
       </div>
